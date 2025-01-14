e9425a037b5a0ff3e2d0b868a5d509cce4ab5c1c
Jonathanmasa <jonathanmasa97@gmail.com>
2025-01-14 17:48:13 +0100

    draft[simon]

---
+++ js/script.js
@@ -0,0 +1,81 @@
+// variabili globali
+console.log("EHI");
+
+
+// numeri casuali generati
+let randomNumbers = [];
+
+// tempo rimanente timer
+let countdown = 5;
+
+
+// selezioni gli elementi html
+const countdownElement = document.getElementById('countdown');
+const numbersList = document.getElementById('numbers-list');
+const answerForm = document.getElementById('answer-form');
+const messageElement = document.getElementById('message');
+const submitButton = document.querySelector('#answer-form button');
+
+
+
+// genero 5 numeri casuali
+function generateRandomNumbers() {
+    const numbers = new Set();
+    while (numbers.size < 5) {
+        // imposto numeri casuali da 1 a 50
+        const randomNumber = Math.floor(Math.random() * 50) + 1;
+        numbers.add(randomNumber);
+    }
+    // salvo i numeri nella variabile globale
+    randomNumbers = Array.from(numbers);
+}
+
+
+// mostro i numeri random in pagina
+function displayNumbers() {
+    
+    numbersList.innerHTML = randomNumber.map(num => `<li>${num}</li>`).join('');
+    
+}
+
+// nascondo numeri e mostro il form
+function toggleVisibility() {
+    numbersList.classList.add('d-none');
+    answerForm.classList.remove('d-none');
+}
+
+// valuto risposta utente
+function checkAnswer() {
+    const userInputs = Array.from(document.querySelectorAll('#answer-form input'))
+          .map(input => parseInt(input.value) || 0);
+    const correctNumbers = userInputs.filter(num => randomNumbers.includes(num));
+    const message = `hai vinto ${correctNumbers.length} numeri: ${correctNumbers.join(', ')}`;
+    messageElement.textContent = message;
+}
+
+// resetto gioco
+function initializeGame() {
+    
+    generateRandomNumbers();
+    displayNumbers();
+}
+
+// timer 5 secondi
+const timer = setInterval(() => {
+    countdown--;
+    countdownElement.textContent = `tempo rimanente: ${countdown}s`;
+    if (countdown <= 0) {
+        clearInterval(timer);
+        toggleVisibility();
+    }
+}, 1000);
+
+// submit del form
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    checkAnswer();
+});
+
+
+// eseguo gioco
+document.addEventListener('DOMContentLoaded', initializeGame);
